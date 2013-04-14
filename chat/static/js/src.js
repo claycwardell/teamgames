065b5679057b6ed0089eb913ff0fad61f85c5e21
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2013-04-13 23:12:28 -0400

    changes

--- chat/static/js/src.js
+++ chat/static/js/src.js
@@ -1,19 +1,42 @@
-// Enable pusher logging - don't include this in production
-Pusher.log = function(message) {
-    if (window.console && window.console.log) window.console.log(message);
+function init(){
+    $('body').addClass(team);
+    // Enable pusher logging - don't include this in production
+    Pusher.log = function(message) {
+        if (window.console && window.console.log) window.console.log(message);
+    };
+
+
+    var pusher = new Pusher('ae35d633bac49aecadaf');
+    var channel = pusher.subscribe('herp');
+    channel.bind('derp', function(data) {
+        $('#eat-my-shorts').append(data.name+': '+ data.message+ '<br />');
+
+    });
+
+    start_request_username()
+
 };
 
+init();
 
-var pusher = new Pusher('ae35d633bac49aecadaf');
-var channel = pusher.subscribe('herp');
-channel.bind('derp', function(data) {
-    $('#eat-my-shorts').append(data.name+': '+ data.message+ '<br />');
 
-});
-$('#submit_button').on('click', function(e){
-    var that = this;
+
+function get_username(){
+    return window.username;
+}
+function on_submit_message_click(e){
+    // check if we have a username
+    if( !get_username() ){
+        start_request_username();
+    }
+    // submit via post request
+    else{
+        submit_message();
+    }
+}
+function submit_message(){
+    var that = this
     var $textbox = $('#text-input');
-    //submit shit to host
     $.ajax({
         "type":"POST",
         "url":"./send_message",
@@ -22,6 +45,47 @@ $('#submit_button').on('click', function(e){
             message:$textbox.val()
         }
     });
-});
+}
 
-$('body').addClass(team);
+function on_submit_username_click(){
+
+    var username = $('#username-input').val();
+    $.ajax({
+        "type":"POST",
+        "url":"./set_username/",
+        data: JSON.stringify({
+            username:username
+        }),
+        success: function(response){
+            // set username
+            window.username = username;
+            window.current_popup.remove();
+            window.current_popup = undefined;
+        },
+        error: function(one, two, three){
+            // username taken, try again
+            var a = 1;
+        }
+    })
+}
+
+function start_request_username(){
+    // create popup
+    window.current_popup = $('' +
+        '<div id="popup-wrapper">' +
+            '<div id="popup">' +
+                '<p>Please enter a username</p>' +
+                '<input id="username-input" type="text">' +
+                '<button id="submit_username">Submit</button> ' +
+            '</div> ' +
+        '</div>');
+    $('body').append(current_popup);
+
+    //
+}
+
+// event bindings
+$('#submit_message').on('click', on_submit_message_click);
+$('#submit_username').on('click', on_submit_username_click);
+
+
