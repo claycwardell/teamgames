7f46e8a15fa904aecb5e02d520084a88eee42008
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2013-04-13 23:31:11 -0400

    changes

--- chat/static/js/src.js
+++ chat/static/js/src.js
@@ -9,7 +9,7 @@ function init(){
     var pusher = new Pusher('ae35d633bac49aecadaf');
     var channel = pusher.subscribe('herp');
     channel.bind('derp', function(data) {
-        $('#eat-my-shorts').append(data.name+': '+ data.message+ '<br />');
+        $('#chat-box').append(data.name+': '+ data.message+ '<br />');
 
     });
 
@@ -36,14 +36,15 @@ function on_submit_message_click(e){
 }
 function submit_message(){
     var that = this
-    var $textbox = $('#text-input');
+    var message = $('#text-input').val();
+    var username = get_username();
     $.ajax({
         "type":"POST",
-        "url":"./send_message",
-        data: {
-            user:that.user,
-            message:$textbox.val()
-        }
+        "url":"./new_message/",
+        data: JSON.stringify({
+            username:username,
+            message:message
+        })
     });
 }
 
